5a6041985cfbb45753dc127c3b221360ae3adc1a
SEOCL <101075938+SEOCL@users.noreply.github.com>
2022-03-07 15:25:42 +0900

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,29 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+import requests
+from bs4 import BeautifulSoup
+
+from pymongo import MongoClient
+client = MongoClient('mongodb+srv://test:sparta@cluster0.ikzrb.mongodb.net/Cluster0?retryWrites=true&w=majority')
+db = client.caffe
+
+@app.route("/caffe", methods=["POST"])
+def caffe_post():
+    name_receive = request.form['name_give']
+    score_receive = request.form['score_give']
+    add_receive = request.form['add_give']
+    add_num = request.form['num_give']
+#
+#     # 일단 망고 플레이트- 필터 - 서울(강남) - 카페에 있는 걸 몽고 db에 직접 추가, 연결 완료, 각 개체 받는 것까지만 완료.
+#     # 카페의 db 경로는 collection name = caffe / 서울은 caffe / 경기는 Gyeonggi
+
+@app.route("/Gyeonggi", methods=["POST"])
+def Gyeonggi_post():
+    name_receive = request.form['name_give']
+    score_receive = request.form['score_give']
+    add_receive = request.form['add_give']
+    add_num = request.form['num_give']
 
 @app.route('/')
 def main():
