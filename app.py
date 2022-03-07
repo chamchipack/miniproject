afc27bb3c76450d52d551528f39b829cafb286a1
SEOCL <101075938+SEOCL@users.noreply.github.com>
2022-03-07 16:23:35 +0900

    Update app.py

--- app.py
+++ app.py
@@ -10,8 +10,8 @@ from pymongo import MongoClient
 client = MongoClient('mongodb+srv://test:sparta@cluster0.ikzrb.mongodb.net/Cluster0?retryWrites=true&w=majority')
 db = client.caffe
 
-@app.route("/caffe", methods=["POST"])
-def caffe_post():
+@app.route("/caffe", methods=["GET"])
+def caffe_get():
     name_receive = request.form['name_give']
     score_receive = request.form['score_give']
     add_receive = request.form['add_give']
@@ -21,8 +21,8 @@ def caffe_post():
 #     # 일단 망고 플레이트- 필터 - 서울(강남) - 카페에 있는 걸 몽고 db에 직접 추가, 연결 완료, 각 개체 받는 것까지만 완료.
 #     # 카페의 db 경로는 collection name = caffe / 서울은 caffe / 경기는 Gyeonggi
 
-@app.route("/Gyeonggi", methods=["POST"])
-def Gyeonggi_post():
+@app.route("/Gyeonggi", methods=["GET"])
+def Gyeonggi_get():
     name_receive = request.form['name_give']
     score_receive = request.form['score_give']
     add_receive = request.form['add_give']
