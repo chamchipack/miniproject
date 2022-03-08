28966c7f3b4df263cd8f07132cf555dce5625f3b
chamchi <fnjels09@gmail.com>
2022-03-08 13:42:48 +0900

    POST함수 추가, 등록기능 추가

--- app.py
+++ app.py
@@ -15,10 +15,28 @@ def home():
     #
     return render_template('index.html', caffe_list=caffe_list)
 
-@app.route("/Gyeonggi", methods=["GET"])
-def Gyeonggi_get():
-    Gyeonggi_list = list(db.Gyeonggi.find({}, {'_id': False}))
-    return jsonify({'Gyeonggi':Gyeonggi_list})
+#@app.route("/Gyeonggi", methods=["GET"])
+#def Gyeonggi_get():
+#    Gyeonggi_list = list(db.Gyeonggi.find({}, {'_id': False}))
+#    return jsonify({'Gyeonggi':Gyeonggi_list})
+
+@app.route("/Gyeonggi", methods=["POST"])
+def Gyeonggi_post():
+    name_receive = request.form['name_give']
+    address_receive = request.form['address_give']
+    comment_receive = request.form['comment_give']
+    url_receive = request.form['url_give']
+    cafe_list = list(db.caffe.find({}, {'_id': False}))
+    count = len(cafe_list) + 1
+    doc = {
+        'name' : name_receive,
+        'add' : address_receive,
+        'comment' : comment_receive,
+        'img' : url_receive,
+        'num' : count,
+    }
+    db.caffe.insert_one(doc)
+    return jsonify({'Gyeonggi':'저장완료'})
 
 @app.route('/')
 def main():
